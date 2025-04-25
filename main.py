d9576b8dbd1ff8ee64ac7133ada76f40c30ac228
Mehdi Roualy <roualymehdi5@gmail.com>
2025-04-26 01:18:11 +0200

    more dev on Grid

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-from src import NoriNori
+from src.NoriNori import *
 
+def main():
+    N = int(input("entrer la taille du terrain \n"))
+    Nori = NoriNori(N)
+    Nori.initGrid()
+    Nori.grid[0][0][1]
+    Nori.printGrid()
+    print(Nori.isNeighbor((1,3), (1,2)))
+    FileName = str(input("what is the file name or storage \n"))
+    Nori.writeGrid(FileName)
+
+
+if __name__ =="__main__":
+    main()
--- src/NoriNori.py
+++ src/NoriNori.py
@@ -1,29 +1,47 @@
 import random
 class NoriNori:
     def __init__(self, size: (int)) -> None:
-        self.grid = [[(int,int)]]
+        self.grid = []
+        self.region = []
         self.size = size
     
     def initGrid(self) -> None:
         for i in range(self.size):
             for j in range(self.size): 
                 self.grid.append([(i, j)])
-        i = self.grid
-    def isNeighbor(self,couple) -> bool:
-        i = self.grid[[]].index(couple)
+
+    def initRegion(self) -> None:
+        print("attend Ahki")
+    
+
+    def isNeighbor(self,coupleCheck: tuple[int, int], coupleNeighbor:tuple[int, int]) -> bool:
+        if ((coupleNeighbor[0] == coupleCheck[0]+1) ^ (coupleNeighbor[1] == coupleCheck[1]+1)) or ((coupleNeighbor[0] == coupleCheck[0]-1) ^ (coupleNeighbor[1] == coupleCheck[1]-1)):
+            return True
+        else:
+            return False       
         
     def printGrid(self) -> None:
         for i in self.grid:
-            if i[0][1] ==0:
+            if i[0][1] == 0:
                 print("\n")
             print(*i, end="")
         print("\n")
 
-    
-
-
-
-if __name__ =="__main__":
-    Nori = NoriNori(6)
+    def writeGrid(self, FileName: str) -> None:
+        Fgrid = open(FileName,"w")
+        Fgrid.write(str(self.size))
+        for i in self.grid:
+            if i[0][1] == 0:
+                Fgrid.write("/")
+                Fgrid.write("\n########## \n")
+            Fgrid.write(str(*i))
+            Fgrid.write(" ")
+        Fgrid.write("f")
+if __name__ == "__main__":
+    N = int(input("entrer la taille du terrain \n"))
+    Nori = NoriNori(N)
     Nori.initGrid()
     Nori.printGrid()
+    print(Nori.isNeighbor((1,3), (1,2)))
+    FileName = input("where is the file")
+    Nori.writeGrid(FileName)
